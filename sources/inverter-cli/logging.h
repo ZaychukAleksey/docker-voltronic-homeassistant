33de5cf8fb8ab7bdc059deca2baa7305d36ac9d4
Aleksey Zaychuk <aleksey.zaychuk@nxlog.org>
2023-11-18 10:01:05 +0300

    Add useful logging-related routines.

--- sources/inverter-cli/logging.h
+++ sources/inverter-cli/logging.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <string>
+#include <string_view>
+
+
 /// Switch to debug mode so all debug logs are printed to the output.
 void SetDebugMode();
+bool IsInDebugMode();
 
+void log(const char* format, ...);
 /// Only print if debug flag is set (see SetDebugMode()), else do nothing.
 void dlog(const char* format, ...);
+
+std::string PrintBytesAsHex(std::string_view str);
+std::string EscapeString(std::string_view src);
