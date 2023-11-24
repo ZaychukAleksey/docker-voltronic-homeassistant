dfaca334655c0780dcec8934cf0767acf9234534
Aleksey Zaychuk <aleksey.zaychuk@nxlog.org>
2023-11-24 10:46:30 +0300

    Use correct icons in mqtt discovery.

--- sources/inverter-mqtt/mqtt-init.sh
+++ sources/inverter-mqtt/mqtt-init.sh
@@ -62,43 +62,48 @@ registerBinarySensor() {
 # $1 - control to register.
 # $2 - device class. Optional. One of https://www.home-assistant.io/integrations/sensor/#device-class
 # $3 - unit of measurement. Optional.
-# $4 - state class. If NOT "None", the sensor is assumed to be numerical and will be displayed as a
-#                   line-chart in the frontend instead of as discrete values.
-#                   If not set, "measurement" will be used by this script.
-#                   https://developers.home-assistant.io/docs/core/entity/sensor/#available-state-classes
+# $4 - icon. Optional. https://www.home-assistant.io/docs/configuration/customizing-devices/#icon
 # https://www.home-assistant.io/integrations/sensor.mqtt/
 registerSensor() {
   CONTROL="$1"
   DEVICE_CLASS="$2"
   UNIT_OF_MEASURE="$3"
-  STATE_CLASS="$4"
+  ICON="$4"
 
-  if [ -z "$2" ]; then
-    # If device class wasn't passed to the function - don't include it into the payload.
+  # If NOT "None", the sensor is assumed to be numerical and will be displayed as a line-chart in
+  # the frontend instead of as discrete values.
+  # https://developers.home-assistant.io/docs/core/entity/sensor/#available-state-classes
+  STATE_CLASS="measurement"
+  if [ -z "$DEVICE_CLASS" ]; then
     DEVICE_CLASS=""
-    UNIT_OF_MEASURE=""
+    STATE_CLASS=""
   else
     DEVICE_CLASS="
   \"device_class\":\"$DEVICE_CLASS\","
-    if [ -z "$3" ]; then
-      # If measurement units wasn't passed to the function - don't include it into the payload.
-      UNIT_OF_MEASURE=""
-    else
-      UNIT_OF_MEASURE=",
-  \"unit_of_measurement\": \"$UNIT_OF_MEASURE\""
-    fi
+    STATE_CLASS="
+  \"state_class\": \"$STATE_CLASS\","
   fi
 
-  if [ -z "$4" ]; then
-    # If state class wasn't passed to the function - use "measurement"
-    STATE_CLASS="measurement"
+  # If measurement units wasn't passed to the function - don't include it into the payload.
+  if [ -z "$UNIT_OF_MEASURE" ]; then
+    UNIT_OF_MEASURE=""
+  else
+    UNIT_OF_MEASURE=",
+  \"unit_of_measurement\": \"$UNIT_OF_MEASURE\""
+  fi
+
+  # If icon wasn't passed to the function - don't include it into the payload.
+  if [ -z "$ICON" ]; then
+    ICON=""
+  else
+    ICON="
+  \"icon\": \"mdi:$ICON\","
   fi
 
   CONTROL_NAME=`echo "$CONTROL" | tr _ " "` # Replace underscores "_" with whitespaces.
   PAYLOAD="{
-  \"device\":$DEVICE_INFO_JSON,$DEVICE_CLASS
-  \"name\": \"$CONTROL_NAME\",
-  \"state_class\": \"$STATE_CLASS\",
+  \"device\":$DEVICE_INFO_JSON,$DEVICE_CLASS $ICON
+  \"name\": \"$CONTROL_NAME\", $STATE_CLASS
   \"state_topic\": \"$MQTT_DISCOVERY_PREFIX/sensor/$DEVICE_ID/$CONTROL\",
   \"unique_id\": \""$DEVICE_SERIAL_NUMBER"_$CONTROL\"$UNIT_OF_MEASURE}"
 
@@ -106,38 +111,38 @@ registerSensor() {
 }
 
 # Info about grid.
-registerSensor "Grid_voltage" "voltage"
+registerSensor "Grid_voltage" "voltage" "V"
 registerSensor "Grid_frequency" "frequency" "Hz"
 
 # Info about the output.
-registerSensor "Output_voltage" "voltage"
+registerSensor "Output_voltage" "voltage" "V"
 registerSensor "Output_frequency" "frequency" "Hz"
-registerSensor "Output_apparent_power" "apparent_power"
+registerSensor "Output_apparent_power" "apparent_power" "VA"
 registerSensor "Output_active_power" "power" "W"
 registerSensor "Output_load_percent" "None" "%"
 
 # Info about batteries
 ## TODO: make it https://www.home-assistant.io/integrations/select.mqtt/
 registerSensor "Battery_type"
-registerSensor "Battery_capacity" "battery"
-registerSensor "Battery_voltage" "voltage"
-registerSensor "Battery_voltage_from_SCC" "voltage"
-registerSensor "Battery_voltage_from_SCC2" "voltage"
-registerSensor "Battery_discharge_current" "current" "A"
-registerSensor "Battery_charge_current" "current" "A"
-registerSensor "Battery_nominal_voltage" "voltage"
-registerSensor "Battery_under_voltage" "voltage"
-registerSensor "Battery_float_voltage" "voltage"
-registerSensor "Battery_bulk_voltage" "voltage"
-registerSensor "Battery_stop_discharging_voltage_with_grid" "voltage"
-registerSensor "Battery_stop_charging_voltage_with_grid" "voltage"
+registerSensor "Battery_capacity" "battery" "%"
+registerSensor "Battery_voltage" "voltage" "V" "current-dc"
+registerSensor "Battery_voltage_from_SCC" "voltage" "V" "current-dc"
+registerSensor "Battery_voltage_from_SCC2" "voltage" "V" "current-dc"
+registerSensor "Battery_discharge_current" "current" "A" "current-dc"
+registerSensor "Battery_charge_current" "current" "A" "current-dc"
+registerSensor "Battery_nominal_voltage" "voltage" "V" "current-dc"
+registerSensor "Battery_under_voltage" "voltage" "V" "current-dc"
+registerSensor "Battery_float_voltage" "voltage" "V" "current-dc"
+registerSensor "Battery_bulk_voltage" "voltage" "V" "current-dc"
+registerSensor "Battery_stop_discharging_voltage_with_grid" "voltage" "V" "current-dc"
+registerSensor "Battery_stop_charging_voltage_with_grid" "voltage" "V" "current-dc"
 
 # PV (Photovoltaics, i.e. solar panels) data.
 registerSensor "PV_watts" "power" "W"
 registerSensor "PV2_watts" "power" "W"
-registerSensor "PV_voltage" "voltage"
-registerSensor "PV2_voltage" "voltage"
-registerSensor "PV_bus_voltage" "voltage"
+registerSensor "PV_voltage" "voltage" "V" "current-dc"
+registerSensor "PV2_voltage" "voltage" "V" "current-dc"
+registerSensor "PV_bus_voltage" "voltage" "V" "current-dc"
 
 # Mode & status & priorities
 ## TODO: these https://www.home-assistant.io/integrations/select.mqtt/
@@ -146,9 +151,9 @@ registerSensor "Out_source_priority"
 registerSensor "Charger_source_priority"
 
 # Various info.
-registerSensor "Heatsink_temperature" "temperature"
-registerSensor "Mptt1_charger_temperature" "temperature"
-registerSensor "Mptt2_charger_temperature" "temperature"
+registerSensor "Heatsink_temperature" "temperature" "°C"
+registerSensor "Mptt1_charger_temperature" "temperature" "°C"
+registerSensor "Mptt2_charger_temperature" "temperature" "°C"
 # TODO: find out how these will be handled. Like whether the inverter accumulates warnings and
 #  returns them once (and then "resets" them), or warnings aren't accumulated and shown only to
 #  reflect the current situation.
@@ -156,4 +161,4 @@ registerSensor "Mptt2_charger_temperature" "temperature"
 
 # Add a separate topic so we can send raw commands from HomeAssistant back to the inverter via MQTT
 # (such as changing power modes etc.).
-registerSensor "COMMANDS" "" "" "None"
+registerSensor "COMMANDS"
